50e39f187428879bf7e1d091d56f934371e788f6
YR1017fr <a27536625@gmail.com>
2021-09-20 16:32:28 +0800

    modify knex

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image=require('./controllers/image');
 const db=knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-contoured-64795',
       port : 5432,
       user : 'postgres',
       password : 'alex134679852',
