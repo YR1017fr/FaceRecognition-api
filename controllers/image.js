ef6b7cdb213f12d97cd5de120b0f1cfcc158f373
YR1017fr <a27536625@gmail.com>
2021-12-12 22:30:07 +0800

    test

--- controllers/image.js
+++ controllers/image.js
@@ -12,6 +12,7 @@ const handleApiCall=(req,res)=>{
 
 const handleImage=(req,res,db)=>{
     const {id} = req.body;
+    console.log('id=',id);
     db('users').where('id','=',id)
     .increment('entries',1)
     .returning('entries')
