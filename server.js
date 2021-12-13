44eb302adf97a88ef5aed628148b1f61623e8e4f
YR1017fr <a27536625@gmail.com>
2021-12-13 22:23:31 +0800

    test

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
-const register= require('./controllers/register');
+const register = require('./controllers/register');
 const signin = require('./controllers/signin');
-const profile= require('./controllers/profile');
-const image=require('./controllers/image');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 var reqTimer = setTimeout(function wakeUp() {
   request("https://nameless-gorge-19527.herokuapp.com", function() {
@@ -30,23 +30,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-  return db.select('*').from('users')                      
-    .then(user=>{
-      res.json(user[0])
-    })
-    .catch(err=>res.status(400).json('unable to get uesr'))
+app.get('/',(req, res) =>{
+  return res.json('work')
 })
 
-app.post('/signin',(req,res)=>signin.handleSignin(req,res,db,bcrypt))
+app.post('/signin',(req, res) =>signin.handleSignin(req, res, db, bcrypt))
 
-app.post('/register',(req,res)=>register.handleRegister(req,res,db,bcrypt))
+app.post('/register',(req, res) =>register.handleRegister(req, res, db, bcrypt))
 
-app.get('/profile/:id',(req,res)=>profile.handleProfile(req,res,db))
+app.get('/profile/:id',(req, res) =>profile.handleProfile(req, res, db))
 
-app.put('/image',(req,res)=>image.handleImage(req,res,db))
+app.put('/image',(req, res) =>image.handleImage(req, res, db))
 
-app.post('/imageurl',(req,res)=>image.handleApiCall(req,res))
+app.post('/imageurl',(req, res) =>image.handleApiCall(req, res))
 
 
 const port = process.env.PORT || 3001;
