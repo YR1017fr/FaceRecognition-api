dd08789e92a638cc857ca09b3ca397797470c6a3
YR1017fr <a27536625@gmail.com>
2021-12-13 16:29:05 +0800

    update api test

--- controllers/image.js
+++ controllers/image.js
@@ -7,36 +7,32 @@ metadata.set("authorization", "Key af090c2f17f1445dbae7b95f10ebcdcf");
 
 
 const Clarifai = require('clarifai');
-console.log(Clarifai);
 const app = new Clarifai.App({
     apiKey: 'af090c2f17f1445dbae7b95f10ebcdcf'
    });
 
 const handleApiCall = (req, res) =>{
-    // stub.PostModelOutputs(
-    //     {
-    //         model_id: "{THE_MODEL_ID}",
-    //         version_id: "{THE_MODEL_VERSION_ID}",  // This is optional. Defaults to the latest model version.
-    //         inputs: [
-    //             {data: {image: {url: req.body.input}}}
-    //         ]
-    //     },
-    //     metadata,
-    //     (err, response) => {
-    //         if (err) {
-    //             throw new Error(err);
-    //         }
+    stub.PostModelOutputs(
+        {
+            model_id: "a403429f2ddf4b49b307e318f00e528b",
+            version_id: "{THE_MODEL_VERSION_ID}",  // This is optional. Defaults to the latest model version.
+            inputs: [
+                {data: {image: {url: req.body.input}}}
+            ]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                throw new Error(err);
+            }
     
-    //         if (response.status.code !== 10000) {
-    //             throw new Error("Post model outputs failed, status: " + response.status.description);
-    //         }
-    //         const output = response.outputs[0];
-    //         return res.json(output);
-    //     }
-    // );
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
-    .then(data =>res.json(data))
-    .catch(err =>res.status(400).json('unable to work with API'))
+            if (response.status.code !== 10000) {
+                throw new Error("Post model outputs failed, status: " + response.status.description);
+            }
+            const output = response.outputs[0];
+            return res.json(output);
+        }
+    );
    }
 
 
